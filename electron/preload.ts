302cb5f13d549ae652f094380567bbcd75cfcfdb
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-30 15:51:08 +0530

    feat:Add preload.ts

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,17 +1,31 @@
 import { contextBridge, ipcRenderer } from 'electron';
-import { SystemAllMetrics, SystemSpecs } from '../src/types';
+import type { SystemAllMetrics, SystemSpecs } from '../shared/types.js';
+
+console.log('[Preload] Loading preload bridge');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  getSystemMetrics: (): Promise<SystemAllMetrics> => ipcRenderer.invoke('get-system-metrics'),
-  getSystemSpecs: (): Promise<SystemSpecs> => ipcRenderer.invoke('get-system-specs'),
-  
+  getSystemMetrics: (): Promise<SystemAllMetrics> => {
+    console.log('[Preload] Renderer requested getSystemMetrics');
+    return ipcRenderer.invoke('get-system-metrics');
+  },
+  getSystemSpecs: (): Promise<SystemSpecs> => {
+    console.log('[Preload] Renderer requested getSystemSpecs');
+    return ipcRenderer.invoke('get-system-specs');
+  },
+
   onMetricsUpdate: (callback: (metrics: SystemAllMetrics) => void) => {
-    const listener = (_event: any, metrics: SystemAllMetrics) => callback(metrics);
+    console.log('[Preload] Renderer subscribed to metrics-update');
+    const listener = (_event: unknown, metrics: SystemAllMetrics) => {
+      console.log('[Preload] metrics-update received by preload bridge');
+      callback(metrics);
+    };
+
     ipcRenderer.on('metrics-update', listener);
-    
-    // Return unsubscribe function
+
     return () => {
       ipcRenderer.removeListener('metrics-update', listener);
     };
-  }
+  },
 });
+
+console.log('[Preload] ContextBridge exposed');
