4b0ed24cdbec2155cb0dfc2d62181c4e889f08f5
Aditya Udai <adityaudai.1305@gmail.com>
2026-06-30 15:58:57 +0530

    feat: Enhance logging in preload bridge for system metrics and specs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,11 +1,25 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+console.log('[Preload] Loading preload bridge');
+
 contextBridge.exposeInMainWorld('electronAPI', {
-  getSystemMetrics: () => ipcRenderer.invoke('get-system-metrics'),
-  getSystemSpecs: () => ipcRenderer.invoke('get-system-specs'),
+  getSystemMetrics: () => {
+    console.log('[Preload] Renderer requested getSystemMetrics');
+    return ipcRenderer.invoke('get-system-metrics');
+  },
+  getSystemSpecs: () => {
+    console.log('[Preload] Renderer requested getSystemSpecs');
+    return ipcRenderer.invoke('get-system-specs');
+  },
   onMetricsUpdate: (callback) => {
-    const listener = (_event, metrics) => callback(metrics);
+    console.log('[Preload] Renderer subscribed to metrics-update');
+    const listener = (_event, metrics) => {
+      console.log('[Preload] metrics-update received by preload bridge');
+      callback(metrics);
+    };
     ipcRenderer.on('metrics-update', listener);
     return () => ipcRenderer.removeListener('metrics-update', listener);
   },
 });
+
+console.log('[Preload] ContextBridge exposed');
